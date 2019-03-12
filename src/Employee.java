916a44bbb927eb19d32a61c2751e1473ffa83e36
Michael Genenko <michaelgenenko@Michaels-MacBook-Pro.local>
2019-03-12 16:03:26 +0300

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,30 @@
+public class Employee {
+    // class variables
+    private int empId;
+    private String name;
+    private Vehicle vehicle;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+}
---
+++ src/PayrollSystem.java
@@ -0,0 +1,5 @@
+public class PayrollSystem {
+    public static void main(String[] args) {
+
+    }
+}
